a34889d30c26f4232d133d9c3bcf872038779612
aarushi2146 <106542499+aarushi2146@users.noreply.github.com>
2025-03-05 12:00:20 +0530

    UC11 with email Services

---
+++ Spring_IntroThree-UC11-emailServices/Spring_IntroThree/src/test/java/com/example/Spring_IntroThree/SpringIntroThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_IntroThree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
